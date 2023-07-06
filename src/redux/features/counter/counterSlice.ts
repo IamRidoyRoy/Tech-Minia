6864b0c5130df749ab432fa246fe046eae48def5
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-07-06 23:03:00 +0600

    Create a slice

---
+++ src/redux/features/counter/counterSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "count",
+  initialState: { count: 0 },
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
+});
