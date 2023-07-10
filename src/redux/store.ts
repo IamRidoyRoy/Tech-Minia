92a5437c39084c330cc6365a02675bc20d9af701
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-07-11 02:51:54 +0600

    Product filter using Redux - price range, stock

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from './features/counter/counterSlice';
 import cartReducer from './features/cart/cartSlice';
+import productReducer from './features/products/productSlice';
 import logger from './middlewares/logger';
 const store = configureStore({
   reducer: {
-    // counter: counterReducer,
     cart: cartReducer,
+    product: productReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
